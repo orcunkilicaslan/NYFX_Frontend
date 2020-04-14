22ef530efb3ca03c378c5d4e579ccb09d32e22a4
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-04-14 03:47:30 +0300

    Homepage Working

--- gulpfile.js
+++ gulpfile.js
@@ -81,6 +81,7 @@ gulp.task('sass-plugin', () => {
 gulp.task('script-plugin', () => {
     return gulp.src([
         folder_src_main + 'script/plugin/**/app.js',
+        folder_src_main + 'script/plugin/**/aos-app.js',
         folder_src_main + 'script/plugin/**/plugin-browser-detection.js'
     ])
         .pipe(plumber())
